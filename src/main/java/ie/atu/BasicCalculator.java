fe77a5a045f199551535a7ca102e2a6aa4c25246
Lim Zhi Lit <g00440629@atu.ie>
2025-09-22 10:04:57 +0100

    Completed

--- src/main/java/ie/atu/BasicCalculator.java
+++ src/main/java/ie/atu/BasicCalculator.java
@@ -15,5 +15,49 @@ public class BasicCalculator {
         double secondNum = scan.nextDouble();
         System.out.printf("You entered %s %n%n", secondNum);
 
+        scan.nextLine();
+
+        System.out.print("Choose the operation (add, subtract, multiply, divide): ");
+        String operation = scan.nextLine();
+        while (!operation.equals("add") && !operation.equals("subtract") && !operation.equals("multiply") && !operation.equals("divide")) {
+            System.out.print("Invalid input,");
+            System.out.print(" choose the operation (add, subtract, multiply, divide): ");
+            operation = scan.nextLine();
+        }
+        System.out.printf("You entered %s %n%n", operation);
+
+        boolean isError = false;
+        double result = switch (operation) {
+            case "add" -> {
+                yield (firstNum + secondNum);
+            }
+            case "subtract" -> firstNum - secondNum;
+            case "multiply" -> firstNum * secondNum;
+            case "divide" -> {
+                if (secondNum == 0) {
+                    System.out.println("Cannot divide by zero");
+                    isError = true;
+                    yield 0;
+                } else {
+                    yield(firstNum / secondNum);
+                }
+            }
+            default -> {
+                System.out.println("Invalid operation");
+                isError = true;
+                yield 0;
+            }
+        };
+
+        if (isError) {
+            System.out.print("Operation error\n");
+            isError = false;
+        } else {
+            System.out.printf("The result is %s %n", result);
+        }
+
+
+
+
     }
 }
